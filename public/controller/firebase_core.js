aaef0ead9bd11a3b294212a074b24cdeb1f7a479
arinck <drinck@uco.edu>
2024-09-04 22:24:04 -0500

    init commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADgf2kicitTw93nKr-2XoXaX2UD8YSyEI",
+    authDomain: "drinck-4373.firebaseapp.com",
+    projectId: "drinck-4373",
+    storageBucket: "drinck-4373.appspot.com",
+    messagingSenderId: "210652733447",
+    appId: "1:210652733447:web:2af53e5e17107ba2d7caa2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
